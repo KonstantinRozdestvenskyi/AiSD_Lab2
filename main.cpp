66a7f40c76caba2b96e9e5d397397e3d4362e951
Костя Рождественский <kosta7497@gmail.com>
2022-10-09 00:40:54 +0300

    change

--- main.cpp
+++ main.cpp
@@ -1,64 +1,114 @@
+//main.cpp
 #include"snail.h"
 #include<algorithm>
 #include<math.h>
-const int N = 1000000;
+#include<time.h>
+const int N = 10000000;
 bool cmp_x(const snail& snail1, const snail& snail2) { //comparator for x
 	return snail1.x < snail2.x || snail1.x == snail2.x && snail1.y < snail2.y;
 }
 bool cmp_y(const snail& snail1, const snail& snail2) { //comparator for y
 	return snail1.y < snail2.y;
 }
-double calculat_dist(const snail& snail1, const snail& snail2) {//auxiliary function that calculates distance between the snails
-	return sqrt((snail1.x - snail2.x) * (snail1.x - snail2.x) + (snail1.y - snail2.y) * (snail1.y - snail2.y));
-}
-double corner(const snail& snail1, const snail& snail2) {
-	return (snail1.x * snail2.x + snail1.y * snail2.y) /()
-}
 double mindist;
-void upd_ans(const snail& snail1, const snail& snail2) {//auxiliary function that calculates minimal distance between the snails
+int upd_ans(const snail& snail1, const snail& snail2) {//auxiliary function that calculates minimal distance between the snails
 	double dist = sqrt((snail1.x - snail2.x) * (snail1.x - snail2.x) + (snail1.y - snail2.y) * (snail1.y - snail2.y));
-	if (dist < mindist)
+	if (dist < mindist) {
 		mindist = dist;
+		return 2;
+	}
+	else
+		if (dist == mindist) {
+			return 1;
+		}
+		else
+			return 0;
 }
 snail quad[N];
-void rec(int l, int r) {
+snail t[N];
+int is_ans_is = 0;
+int u = 0;
+void find(int l, int r) {
 	if (r - l <= 3) {
-		for (int i = l; i <= r; ++i)
+		for (int i = l; i <= r; ++i) {
 			for (int j = i + 1; j <= r; ++j)
-				upd_ans(quad[i], quad[j]);
+				if (upd_ans(quad[i], quad[j]) == 1)
+					u++;
+				else
+					u = 1;
+			if (u > 1)
+				is_ans_is = 0;
+			else
+				is_ans_is = 1;
+		}
 		sort(quad + l, quad + r + 1, &cmp_y);
-		return;
+		return ;
 	}
 
 	int m = (l + r) >> 1;
 	int m_x = quad[m].x;
-	rec(l, m), rec(m + 1, r);
-	static snail t[N];
-	merge(quad + l, quad + m + 1, quad + m + 1, quad + r + 1, t, &cmp_y);
-	copy(t, t + r - l + 1, quad + l);
+	find(l, m);
+	double h1 = mindist;
+	find(m + 1, r);
+	double h2 = mindist;
+	double h = min(h1, h2);
+	if (h < mindist)
+		mindist = h;
 
 	int tsz = 0;
-	for (int i = l; i <= r; ++i)
-		if (abs(quad[i].x - m_x) < mindist) {
+	for (int i = l; i <= r; ++i) {
+		u = 0;
+		if (abs(quad[i].x - m_x) <= mindist) {
 			for (int j = tsz - 1; j >= 0 && quad[i].y - t[j].y < mindist; --j)
-				upd_ans(quad[i], t[j]);
+				if (upd_ans(quad[i], t[j]) == 1)
+					u++;
+				else
+					u = 1;
 			t[tsz++] = quad[i];
 		}
+		if (u > 1)
+			is_ans_is = 0;
+		else
+			is_ans_is = 1;
+	}
 }
 int main() {
 	cout << "enter the number of snails" << '\n';
 	int number;
 	cin >> number;
-	cout << "enter the coordinates of the snails" << '\n';
+	cout << "enter the coordinates of the snails" << '\n'; 
 	for (int i = 0; i < number; i++) {
 		cin >> quad[i].x >> quad[i].y;
 	}
 	sort(quad, quad + number, &cmp_x);
-	for (int i = 1; i < number - 1; i++) {
-
-	}
 	mindist = 1E20;
-	rec(0, number - 1);
-	cout << "the minimum time is equal to " << mindist << '\n';
+	find(0, number - 1);
+	if (is_ans_is == 1) {
+		cout << "\nthe minimum time is equal to " << mindist << '\n';
+	}
+	else
+		cout << "\nno answer." << '\n';
+	/*int number = 0;        //a fragment for empirical evaluation
+	setlocale(LC_ALL, "RUS");
+	while (number <= 10000) {
+		for (int i = 0; i < number; i++) {
+			quad[i].x = 0 + rand() % 10001;
+			quad[i].y = 0 + rand() % 10001;
+		}
+		const clock_t start = clock();
+		sort(quad, quad + number, &cmp_x);
+		mindist = 1E20;
+		rec(0, number - 1);
+		const clock_t end = clock();
+		const double time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
+		cout << '(' << number << ';' << time << ')';
+		number += 100;
+		if (is_ans_is == 1) {
+		cout << "\nthe minimum time is equal to " << mindist << '\n';
+	}
+	else
+		cout << "\nno answer." << '\n';
+
+	}*/
 	return 0;
 }
--- snail.h
+++ snail.h
@@ -1,3 +1,4 @@
+//smail.h
 #pragma once
 #include<iostream>
 using namespace std;
